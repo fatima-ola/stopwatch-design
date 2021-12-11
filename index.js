c91d6af55f43ffc8fa069bce108fa6631f4b08fc
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-12-11 20:57:36 +0100

    structured and style the timer section

--- index.js
+++ index.js
@@ -14,74 +14,75 @@ homeBtn.addEventListener("click", function() {
 
 
 
-// // setting timer variable to zero
-// var timer = 0;
-// console.log(timer);
-// // declare timer interval but value isn't declared
-// var intervalValue;
+// setting timer variable to zero
+var timer = 0;
+console.log(timer);
+// declare timer interval but value isn't declared
+var intervalValue;
 
-// // declare const for minutes, seconds and milliseconds counters
-
-
-// var minutesNotation = document.querySelector(".minutes");
-// var secondsNotation = document.querySelector(".seconds");
-// var milliSecondsNotation = document.querySelector(".milli-seconds");
+// declare const for minutes, seconds and milliseconds counters
 
 
-// console.log(minutesNotation, secondsNotation, milliSecondsNotation);
-
-// function startTimer() {
-//     stop();
-
-//     intervalValue = setInterval(function() {
-//             timer += 1 / 60;
-//             milliSecondsValue = Math.floor((timer - Math.floor(timer)) * 100);
-//             secondsValue = Math.floor((timer) - Math.floor(timer / 60) * 60);
-//             minuteValue = Math.floor((timer) / 60);
-
-//             milliSecondsNotation.innerHTML = milliSecondsValue < 10 ? "0" + milliSecondsValue.toString() : milliSecondsValue;
-
-//             secondsNotation.innerHTML = secondsValue < 10 ? "0" + secondsValue.toString() : secondsValue;
-
-//             minutesNotation.innerHTML = minuteValue < 10 ? "0" + minuteValue.toString() : minuteValue;
-//         })
-//         // resetValue();
-
-// }
-
-// function stop() {
-//     clearInterval(intervalValue);
-// }
-
-// function resetValue() {
-//     // minutesNotation.value = "00";
-//     // console.log(minutesNotation.textContent)
-//     // console.log(secondsNotation.textContent)
-//     // minutesNotation.textContent = "0" + "0";
-//     // secondsNotation.innerHTML = "0" + "0";
-
-//     location.reload();
-
-// }
+var minutesNotation = document.querySelector(".minutes");
+console.log(minutesNotation)
+var secondsNotation = document.querySelector(".seconds");
+var milliSecondsNotation = document.querySelector(".milli-seconds");
 
 
+console.log(minutesNotation, secondsNotation, milliSecondsNotation);
 
-    //   var div = document.getElementById("location");
+function startTimer() {
+    stop();
 
-    //   function getLocation() {
-    //     if (navigator.geolocation) {
-    //       navigator.geolocation.getCurrentPosition(showPosition, console.log);
-    //     } else {
-    //       div.innerHTML = "The Browser Does not Support Geolocation";
-    //     }
-    //   }
+    intervalValue = setInterval(function() {
+            timer += 1 / 60;
+            milliSecondsValue = Math.floor((timer - Math.floor(timer)) * 100);
+            secondsValue = Math.floor((timer) - Math.floor(timer / 60) * 60);
+            minuteValue = Math.floor((timer) / 60);
 
-    //   function showPosition(position) {
-    //     var lat = position.coords.latitude;
-    //     var lon = position.coords.longitude;
-    //     // You need to enter your API Key here
-    //     var api_key = "";
-    //     var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&zoom=14&size=400x300&sensor=false&key=${api_key}`;
-    //     div.innerHTML = `<img src='${img_url}'>`;
-    //   }
-    //   getLocation();
+            milliSecondsNotation.innerHTML = milliSecondsValue < 10 ? "0" + milliSecondsValue.toString() : milliSecondsValue;
+
+            secondsNotation.innerHTML = secondsValue < 10 ? "0" + secondsValue.toString() : secondsValue;
+
+            minutesNotation.innerHTML = minuteValue < 10 ? "0" + minuteValue.toString() : minuteValue;
+        })
+        // resetValue();
+
+}
+
+function stop() {
+    clearInterval(intervalValue);
+}
+
+function resetValue() {
+    // minutesNotation.value = "00";
+    // console.log(minutesNotation.textContent)
+    // console.log(secondsNotation.textContent)
+    // minutesNotation.textContent = "0" + "0";
+    // secondsNotation.innerHTML = "0" + "0";
+
+    location.reload();
+
+}
+
+
+
+      var div = document.getElementById("location");
+
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition, console.log);
+        } else {
+          div.innerHTML = "The Browser Does not Support Geolocation";
+        }
+      }
+
+      function showPosition(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        // You need to enter your API Key here
+        var api_key = "";
+        var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&zoom=14&size=400x300&sensor=false&key=${api_key}`;
+        div.innerHTML = `<img src='${img_url}'>`;
+      }
+      getLocation();
